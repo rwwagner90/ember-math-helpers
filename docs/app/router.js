3d41e06c2e99cab91cddc5e7e84aef7dfaf41b95
Robert Wagner <rwwagner90@gmail.com>
2021-08-30 20:49:12 -0400

    Move documentation to /docs

---
+++ docs/app/router.js
@@ -0,0 +1,19 @@
+import AddonDocsRouter, { docsRoute } from 'ember-cli-addon-docs/router';
+import config from 'docs/config/environment';
+
+const Router = AddonDocsRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL,
+});
+
+Router.map(function () {
+  docsRoute(this, function () {
+    this.route('configuration');
+    this.route('playground');
+    this.route('usage');
+
+    this.route('not-found', { path: '/*path' });
+  });
+});
+
+export default Router;
