63cbe31d9d311a2397e686ae3561fc904b7f2518
Aleksey Strizhak <alexei.mifrill.strizhak@gmail.com>
2021-09-24 09:57:38 +0500

    implement sum helper as strict alias for add helper

---
+++ tests/unit/helpers/sum-test.js
@@ -0,0 +1,15 @@
+import { sum } from 'dummy/helpers/sum';
+import { add } from 'dummy/helpers/add';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | sum', function () {
+  test('works as alias to add', function (assert) {
+    const input = [20, 10];
+    assert.strictEqual(sum(input), add(input));
+  });
+
+  test('works as alias to add for multiple arguments', function (assert) {
+    const input = [1, 2, 3, 4, 5];
+    assert.strictEqual(sum(input), add(input));
+  });
+});
