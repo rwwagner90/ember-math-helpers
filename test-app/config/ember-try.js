67e4dd81aa550d36553df3aff7098524085e23bf
Robert Wagner <rwwagner90@gmail.com>
2023-06-19 22:18:05 -0400

    Update try cases

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -8,21 +8,18 @@ module.exports = async function () {
     usePnpm: true,
     scenarios: [
       {
-        name: 'ember-lts-3.28',
+        name: 'ember-lts-4.4',
         npm: {
           devDependencies: {
-            'ember-data': '~3.28.0',
-            'ember-resolver': '^8.0.0',
-            'ember-source': '~3.28.0',
+            'ember-source': '~4.4.0',
           },
         },
       },
       {
-        name: 'ember-lts-4.4',
+        name: 'ember-lts-4.8',
         npm: {
           devDependencies: {
-            'ember-resolver': '^8.0.0',
-            'ember-source': '~4.4.0',
+            'ember-source': '~4.8.0',
           },
         },
       },
@@ -47,25 +44,6 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': await getChannelURL('canary'),
-          },
-        },
-      },
-      {
-        name: 'ember-classic',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'application-template-wrapper': true,
-            'default-async-observers': false,
-            'template-only-glimmer-components': false,
-          }),
-        },
-        npm: {
-          devDependencies: {
-            'ember-resolver': '^8.0.0',
-            'ember-source': '~3.28.0',
-          },
-          ember: {
-            edition: 'classic',
           },
         },
       },
