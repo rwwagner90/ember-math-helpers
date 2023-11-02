dfdc1b71befcbca6b5ef0918b20820dbbd93e3f8
Robbie Wagner <rwwagner90@gmail.com>
2023-11-02 12:32:51 -0400

    Fix pnpm and tests

--- ember-math-helpers/src/helpers/random.ts
+++ ember-math-helpers/src/helpers/random.ts
@@ -1,3 +1,4 @@
+import { isArray } from '@ember/array';
 import { helper } from '@ember/component/helper';
 
 export interface RandomSignature {
@@ -8,8 +9,17 @@ export interface RandomSignature {
   Return: number;
 }
 
+const { min, max } = Math;
+
 // @see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed#max(0, min(MAX_DECIMALS, decimals))));
 const MAX_DECIMALS = 20;
+
+// 💡 Using Number.toFixed, we'll get rounding for free alongside
+// decimal precision. We'll default to whole-number rounding simply
+// by defaulting `decimals` to 0
+const DEFAULT_OPTS = {
+  decimals: 0,
+};
 
 /**
  * Executes `Math.random` with the provided upper-bounded randoms, lower- and upper-bounded randoms,
@@ -27,47 +37,56 @@ const MAX_DECIMALS = 20;
  * {{random 21 1797}} or {{random 21 1797 decimals=4}}
  * ```
  *
- * @param upperBound
- * @param lowerBound
+ * @param {number} upperBound
+ * @param {number} lowerBound
  * @param {object} { decimals } decimal precision value
- * @return The random generated number
+ * @return {number} The random generated number
  */
 export function random(
-  [upperBound, lowerBound]: RandomSignature['Args']['Positional'] = [],
-  { decimals }: RandomSignature['Args']['Named'] = { decimals: 0 },
+  params:
+    | RandomSignature['Args']['Named']
+    | RandomSignature['Args']['Positional'],
+  { decimals }: RandomSignature['Args']['Named'] = DEFAULT_OPTS,
 ) {
   // no positional args, but only an options hash from named args
-  if (typeof upperBound === 'undefined' && typeof lowerBound === 'undefined') {
-    decimals = decimals ?? 0;
+  if (typeof params === 'object' && !isArray(params)) {
+    decimals =
+      typeof params.decimals !== 'undefined'
+        ? params.decimals
+        : DEFAULT_OPTS.decimals;
 
-    return +Math.random().toFixed(
-      Math.max(0, Math.min(MAX_DECIMALS, decimals)),
-    );
+    return +Math.random().toFixed(max(0, min(MAX_DECIMALS, decimals)));
   }
 
   // one positional arg: treat it as an upper bound
-  if (typeof upperBound !== 'undefined' && typeof lowerBound === 'undefined') {
-    return +(Math.random() * upperBound).toFixed(
-      Math.max(0, Math.min(MAX_DECIMALS, decimals ?? 0)),
-    );
+  if (params && params.length === 1) {
+    const [upperBound] = params;
+
+    if (typeof upperBound === 'number') {
+      return +(Math.random() * upperBound).toFixed(
+        max(0, min(MAX_DECIMALS, decimals ?? 0)),
+      );
+    }
   }
 
   // two positional args: use them to derive upper and lower bounds
-  if (typeof upperBound !== 'undefined' && typeof lowerBound !== 'undefined') {
-    // for convenience, swap if a higher number is accidentally passed first
-    if (upperBound < lowerBound) {
-      [lowerBound, upperBound] = [upperBound, lowerBound];
-    }
+  if (params && params.length === 2) {
+    let [lowerBound, upperBound] = params;
 
-    return +(lowerBound + Math.random() * (upperBound - lowerBound)).toFixed(
-      Math.max(0, Math.min(MAX_DECIMALS, decimals ?? 0)),
-    );
+    if (typeof upperBound === 'number' && typeof lowerBound === 'number') {
+      // for convenience, swap if a higher number is accidentally passed first
+      if (upperBound < lowerBound) {
+        [lowerBound, upperBound] = [upperBound, lowerBound];
+      }
+
+      return +(lowerBound + Math.random() * (upperBound - lowerBound)).toFixed(
+        max(0, min(MAX_DECIMALS, decimals ?? 0)),
+      );
+    }
   }
 
   // no positional or named args: just return Math.random() w/ default decimal precision
-  return +Math.random().toFixed(
-    Math.max(0, Math.min(MAX_DECIMALS, decimals ?? 0)),
-  );
+  return +Math.random().toFixed(max(0, min(MAX_DECIMALS, decimals ?? 0)));
 }
 
 export default helper<RandomSignature>(random);
