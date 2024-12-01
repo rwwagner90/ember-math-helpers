6dfca2aab3e683a27af41f0a3212e525565c5182
Robert Wagner <rwwagner90@gmail.com>
2024-12-01 07:18:22 -0500

    Revert "/shrug"
    This reverts commit 5fcdf770d3acefe9197369df869f715296074f7f.

--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -10,7 +10,12 @@ module.exports = function (defaults) {
     },
   });
 
-  const { maybeEmbroider } = require('@embroider/test-setup');
-
-  return maybeEmbroider(app);
+  const { Webpack } = require('@embroider/webpack');
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    skipBabel: [
+      {
+        package: 'qunit',
+      },
+    ],
+  });
 };
