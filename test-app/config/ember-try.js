703fa0b49bdbadc4d17fa93e0af4cf0570b51c65
Robbie Wagner <rwwagner90@gmail.com>
2024-12-03 10:20:06 -0500

    Try adding `--ignore-scripts`

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -6,6 +6,9 @@ const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 module.exports = async function () {
   return {
     usePnpm: true,
+    buildManagerOptions(scenario) {
+      return ['--ignore-scripts'];
+    },
     scenarios: [
       {
         name: 'ember-lts-4.4',
