f4ade6c17ee4033acb71a7da43b0c670f3c8cb00
Robbie Wagner <rwwagner90@gmail.com>
2024-12-03 09:48:12 -0600

    Add back addon-docs (#1851)
    * Add back addon-docs
    
    * Fix lint
    
    * Try adding `--ignore-scripts`
    
    * Fix lint
    
    * Update ember-try.js
    
    * Update ci.yml

--- test-app/.template-lintrc.js
+++ test-app/.template-lintrc.js
@@ -3,8 +3,9 @@
 module.exports = {
   extends: 'recommended',
   rules: {
+    'no-builtin-form-components': false,
     'no-curly-component-invocation': {
-      allow: ['input', 'svg-jar'],
+      allow: ['input', 'render-template', 'svg-jar'],
     },
     // TODO turn this back on when fixed https://github.com/ember-template-lint/ember-template-lint/issues/1135
     'no-quoteless-attributes': false,
