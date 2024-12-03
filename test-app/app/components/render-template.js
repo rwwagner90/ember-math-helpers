b7fe5c14ea23b3e9dc3498446e4ef938f0e5ad2b
Robbie Wagner <rwwagner90@gmail.com>
2024-12-03 16:28:43 -0500

    Try embroider macros

--- test-app/app/components/render-template.js
+++ test-app/app/components/render-template.js
@@ -2,12 +2,21 @@ import Component from '@glimmer/component';
 import { compileHBS } from 'ember-repl/formats/hbs';
 import * as helpers from 'ember-math-helpers';
 import { cached } from '@glimmer/tracking';
+import {
+  macroCondition,
+  dependencySatisfies,
+  importSync,
+} from '@embroider/macros';
 
 export default class RenderTemplate extends Component {
   @cached
   get compileResult() {
-    return compileHBS(this.args.templateString, {
-      scope: { ...helpers },
-    });
+    if (macroCondition(dependencySatisfies('ember-source', '>= 5.0.0'))) {
+      return compileHBS(this.args.templateString, {
+        scope: { ...helpers },
+      });
+    }
+
+    return false;
   }
 }
